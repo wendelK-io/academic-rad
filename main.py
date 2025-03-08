43c15c8542d49a5d36d6a9e50069a77d2886e1e7
wendelK-io <wendell01.bin@gmail.com>
2025-03-08 17:56:44 -0300

    getting file information

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 file = open("sample.txt","r")
 
-# .seek() place the cursor in the desired position
-file.seek(5)
-print(file.readline())
-
-# Return the cursor positioon in bytes
-print(file.tell())
+print(file.mode)
+print(file.name)
+print(file.encoding)
+print(file.closed)

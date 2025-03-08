288078c6964a4494fc8f791b065db511394fee58
wendelK-io <wendell01.bin@gmail.com>
2025-03-08 17:38:07 -0300

    readline(s)

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 file = open("sample.txt","r")
-print(file.read())
+
+# reads one line of the file at a time
+print(file.readline())
+
+# returns a list of strings from each line of the file
+print(file.readlines())

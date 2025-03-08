9ef322d1d9b687fc576d7e0ba576877d346c52ff
wendelK-io <wendell01.bin@gmail.com>
2025-03-08 18:17:37 -0300

    creates/ overwrite file

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-file = open("sample.txt","r")
+file = open("sample.txt","w")
 
-print(file.mode)
-print(file.name)
-print(file.encoding)
+# Create/ overwrite a file
+file.write("Hello, world!")
+file.close()
 print(file.closed)
+
+
+# Lists - case 1
+
+shopping_list_file = open("shopping_list", "w")
+list = ["banana", "apple", "grapes", "breads", "juice"]
+
+for item in list:
+    shopping_list_file.write(str(item) + "\n")
+
+shopping_list_file.close()
+
+shopping_list_file = open("shopping_list", "r")
+print(shopping_list_file.read())
+
+# Lists - case 2
+
+file = open("point_list", "w")
+point_list = [("Bianca", "2.5"), ("Teo", "2"), ("Brenda", "4")]
+
+for student, points in point_list:
+    file.write(f"Student: {student} | Points: {points} \n")
+
+file.close()
+
+file = open("point_list", "r")
+print(file.read())
